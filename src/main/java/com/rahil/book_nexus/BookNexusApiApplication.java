31f63116c299e3d5b9ce64f05a310e17100f87ac
Rahil <rahildesai2003@gmail.com>
2025-07-05 18:57:30 -0400

    Add Spring Boot project structure

---
+++ src/main/java/com/rahil/book_nexus/BookNexusApiApplication.java
@@ -0,0 +1,13 @@
+package com.rahil.book_nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNexusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNexusApiApplication.class, args);
+	}
+
+}
