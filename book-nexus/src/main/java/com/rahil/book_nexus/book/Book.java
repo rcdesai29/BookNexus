2efe3600bf876ef83a5c2e876b8d265db8766a58
Rahil Desai <rahildesai2003@gmail.com>
2025-07-22 18:18:59 -0400

    Completed book folder with functionality

--- book-nexus/src/main/java/com/rahil/book_nexus/book/Book.java
+++ book-nexus/src/main/java/com/rahil/book_nexus/book/Book.java
@@ -1,53 +1,58 @@
 package com.rahil.book_nexus.book;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-import java.time.LocalDate;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import com.rahil.book_nexus.common.BaseEntity;
+import com.rahil.book_nexus.feedback.Feedback;
+import com.rahil.book_nexus.history.BookTransactionHistory;
 import com.rahil.book_nexus.user.User;
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import jakarta.persistence.Column;
 import java.util.List;
-import com.rahil.book_nexus.feedback.Feedback;
-import com.rahil.book_nexus.history.BookTransactionHistory;
 
-@Entity
 @Getter
 @Setter
 @SuperBuilder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Book extends BaseEntity {
 
     private String title;
     private String authorName;
     private String isbn;
-    private String synposis;
+    private String synopsis;
     private String bookCover;
     private boolean archived;
     private boolean shareable;
-
+    @Column(name = "created_by")
+    private String createdBy;
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private User owner;
-
     @OneToMany(mappedBy = "book")
     private List<Feedback> feedbacks;
-
     @OneToMany(mappedBy = "book")
     private List<BookTransactionHistory> histories;
+
+    @Transient
+    public double getRate() {
+        if (feedbacks == null || feedbacks.isEmpty()) {
+            return 0.0;
+        }
+        var rate = this.feedbacks.stream()
+                .mapToDouble(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+        double roundedRate = Math.round(rate * 10.0) / 10.0;
+
+        return roundedRate;
+    }
 }
---
+++ book-nexus/src/main/java/com/rahil/book_nexus/book/BookSpecification.java
@@ -0,0 +1,10 @@
+package com.rahil.book_nexus.book;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public class BookSpecification {
+
+    public static Specification<Book> withOwnerId(String ownerId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("createdBy"), ownerId);
+    }
+}
