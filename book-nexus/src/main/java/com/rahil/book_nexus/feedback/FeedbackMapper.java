50ae5e2cf268e3d3348dc1dde755e23a63b4bb1b
Rahil Desai <rahildesai2003@gmail.com>
2025-07-22 19:07:31 -0400

    Completed feedback funcationality

---
+++ book-nexus/src/main/java/com/rahil/book_nexus/feedback/FeedbackMapper.java
@@ -0,0 +1,29 @@
+package com.rahil.book_nexus.feedback;
+
+import com.rahil.book_nexus.book.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .shareable(false)
+                        .archived(false)
+                        .build())
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer id) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getUser().getId(), id))
+                .build();
+    }
+}
